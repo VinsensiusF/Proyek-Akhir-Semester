6641c050f1dbf7ddb36232bdd37476fac3b985e0
amansanadhifah <amandanadhifah8@gmail.com>
2022-12-03 00:05:30 +0700

    show toko

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -8,8 +8,11 @@ import '../main.dart';
 import 'package:pas/pages/kategori_page.dart';
 import 'package:pas/pages/forum_page.dart';
 
+import '../pages/form_toko.dart';
+import '../pages/kategori_details_page.dart';
+
 class Drawers extends StatefulWidget {
-  const Drawers({ Key? key }) : super(key: key);
+  const Drawers({Key? key}) : super(key: key);
 
   @override
   _DrawerState createState() => _DrawerState();
@@ -20,54 +23,81 @@ class _DrawerState extends State<Drawers> {
   Widget build(BuildContext context) {
     return Drawer(
         child: Padding(
-          padding: const EdgeInsets.all(2),
-          child: Column(
-            children: [
-              ListTile(
-                title: const Text("Home Page"),
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => const MyHomePage(title: 'SellerPrism.io',)),
-                  );
-                },
-              ),
-              ListTile(
-                      title: const Text("Profile"),
-                      onTap: () {
-                        Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => ProfilePage()),
-                        );
-                      },
-                    ),
-                    ListTile(
-                title: const Text("Lihat Kategori"),
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => const MyKategoriPage()),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text("Forum"),
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => const ForumPage()),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('FAQ'),
-                onTap: () {
-                  // Route menu ke halaman to do
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MyFAQPage()),
-                  );
-                },
-              ),
-            ],
+      padding: const EdgeInsets.all(2),
+      child: Column(
+        children: [
+          ListTile(
+            title: const Text("Home Page"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyHomePage(
+                          title: 'SellerPrism.io',
+                        )),
+              );
+            },
           ),
-        )
-    );
+          ListTile(
+            title: const Text("Profile"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text("Lihat Kategori"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyKategoriPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text("Forum"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ForumPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('FAQ'),
+            onTap: () {
+              // Route menu ke halaman to do
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyFAQPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Toko'),
+            onTap: () {
+              // Route menu ke halaman to do
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyKategoriDetailPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Form Toko'),
+            onTap: () {
+              // Route menu ke halaman to do
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const TokoForm()),
+              );
+            },
+          ),
+        ],
+      ),
+    ));
   }
 }
